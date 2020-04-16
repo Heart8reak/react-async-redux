8175acdad52c4dcaaff9344aecde4e869f233d87
Carlos Baez <carlosbaez@Green-Planet.local>
2020-04-15 20:12:17 -0400

    was able to pull data successfully

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -14,18 +14,19 @@ export const getData = () => dispatch => {
     // .get(`https://fmpcloud.io/api/v3/historical-price-full/${COM_TICKER}?apikey=${API_KEY}`)
 
     .get(`https://fmpcloud.io/api/v3/historical-price-full/AAPL?apikey=f516bdb97ed378dea4c9519d856d6742`)
+
     .then(res => {
         console.log('RESPONSE: ', res.data)
         dispatch({
             type: UPDATE_DATA,
             payload: res.data
         })
-        .catch(err => {
-            console.log('ERROR FETCHING DATA FROM API: ', err)
-            dispatch({
-                type: SET_ERROR,
-                payload: 'Error fetching data from the API'
-            })
-        })
+    // .catch(err => {
+    //     console.log('ERROR FETCHING DATA FROM API: ', err)
+    //     dispatch({
+    //         type: SET_ERROR,
+    //         payload: 'Error fetching data from the API'
+    //     })
+    // })
     })
 }
